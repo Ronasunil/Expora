bb1e8a4cb9076e90b9de9fc239768b0d9a8b5720
Ronasunil <ronasunilcoc@gmail.com>
2024-01-10 00:03:09 +0530

    Some change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const webSocketHandler = require("wsManger.js");
+const webSocketHandler = require("./wsManger.js");
 
 process.on("uncaughtException", (err) => {
   console.log(err);
@@ -11,7 +11,7 @@ process.on("uncaughtException", (err) => {
 
 mongoose.connect(process.env.LOCAL_DATABASE);
 
-const app = require("app.js");
+const app = require("./app.js");
 
 const port = process.env.PORT || 3000;
 
